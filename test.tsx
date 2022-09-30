41370fe8b24e0b8099bd039a6a5f5ecdccaf7f62
Tung Duong <duongk22899@gmail.com>
2022-10-01 00:39:47 +0700

    typescript

---
+++ test.tsx
@@ -0,0 +1,15 @@
+import {ChangeEventHandler} from 'react'
+
+interface ISearchBoxProps {
+    className: string,
+    placeHolder?: string
+}
+
+interface ISearchBoxProps {
+    onChangeHandler: ChangeEventHandler
+}
+
+type SearchBoxProps = {
+    className: string
+}
+
